3d73c1672735942c66cea1dafc3dba464c863394
sushantkhaddka20xx <itsmesushant05@gmail.com>
2022-04-19 19:45:48 +0545

    For looping judt finish

--- index.php
+++ index.php
@@ -24,8 +24,78 @@ if($num<10){
 }elseif($num== 10){
     echo "Your are only 10.";
 }elseif($num<=15){
-    echo "You are Marad";
+    echo "You are Marad<br>";
 }
 
+//----Switch case in PHP----
+$weekDay = 5;
 
+switch($weekDay){
+    case 1:{
+        echo "Sunday";
+        break;
+    }case 2:{
+        echo "Monday";
+        break;
+    }case 3:{
+        echo "Tuesday";
+        break;
+    }case 4:{
+        echo "Wednesday";
+        break;
+    }case 5:{
+        echo "Thursday";
+        break;
+    }case 6:{
+        echo "Friday";
+        break;
+    }case 7:{
+        echo "Saturday";
+        break;
+    }default:{
+        echo "wrong Input";
+        break;
+    }
+}
+
+//---String concatination----
+$FirstName = "<br>Sushant";
+$LastName = "Khadka";
+$FullName = $FirstName." ".$LastName."<br>";
+echo $FullName;
+
+//---while Loop in Php--
+$i = 1;
+while($i<=10){
+    echo "Hellow world<br>";
+    $i = $i +1;
+}
+
+//---Do while loop--
+$i = 0;
+do{
+    echo "Hellow world<br>";
+    $i = $i +1;
+}while($i<=10)
 ?>
+
+<?php
+//---For loop--
+for ($a = 1; $a<=10; $a++){
+    echo $a."<br>";
+}
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
