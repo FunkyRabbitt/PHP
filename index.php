719ebdb83247f8b31389c454293cda9417e3d3d7
sushantkhadka20xx <itsmesushant05@gmail.com>
2022-05-11 12:51:18 +0545

    adding the slacess into the word

--- index.php
+++ index.php
@@ -21,8 +21,44 @@
     <?php
     //---Reversing thee word in php
     $fname = "sushant";
-    echo "<br>".strrev($fname); //--reversing the word 
-    echo "<br>".str_shuffle($fname); //-- Suffle the word into the random order
+    echo "<br>" . strrev($fname); //--reversing the word 
+    echo "<br>" . str_shuffle($fname); //-- Suffle the word into the random order
+    ?>
+    <?php
+    //--Giving the padding into the word
+    // used for making the hasing algorithm
+    $fname = "sushant";
+    $length = 100;
+    $sign = "_";
+    echo str_pad($fname, 30, "."); //---gives the padding of 30 character into the previouis word
+    echo "<br>" . str_pad($fname, 30, "!@#$%^&", STR_PAD_BOTH); //--gives the padding from both side equally
+    echo "<br>" . str_repeat($sign, $length); //-Helps to repeat the same value into the given times
+    ?>
+    <?php
+    //--Trimming function in php 
+    //-- helps to trip the given word from the both side
+    $fname = "sushant";
+    echo "<br>" . trim($fname, "st"); //--trim the character between s and t
+    $sname = "   sushant      ";
+    echo "<br>" . $sname;
+    echo "<br>" . trim($sname); //ttrim the blank space from both side
+    ?>
+    <?php
+    //---adding the slacess into the word
+    /// adding the slacces / into the word
+    $str = "my name is 'sushant'";
+    echo "<br>" . $str;
+    echo "<br>" . addslashes($str); //--adding the slacess before court
+    $new = addslashes($str);
+    echo "<br>" . $new;
+    echo "<br>" . stripslashes($new); // removing thw slacess from the word
+
+    //Adding the custom slacess
+    $str = "my name is 'sushant'";
+    $new = addcslashes($str, "a...z"); //--> adding the slacess from the range
+    echo "<br>" . $new;
+    $snew = stripslashes($new); //--Removing all the slacess from the word
+    echo "<br>" . $snew;
     ?>
 </body>
 
